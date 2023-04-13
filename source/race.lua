23a5e38d1dfe15a19e1e66b3e6e35f5787cc19bd
rae <linkisawakening@gmail.com>
2023-04-13 08:22:48 -0700

    adding some comments it accidentally left behind

--- source/race.lua
+++ source/race.lua
@@ -2,12 +2,13 @@
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
+-- Defining some variables we'll need later
 local rowbot_speed = 8
 local boat_rotation = 0
 local rowbot_oar_anim = 1
 local player_oar_anim = 1
 
-
+-- Import all the images!!!!1!
 local img_meter = gfx.image.new('images/meter')
 local img_meter_mask = gfx.image.new('images/meter_mask')
 local meter_image = gfx.image.new(130, 118)
@@ -23,11 +24,13 @@ local img_track_test_fg = gfx.image.new('images/track_test_fg')
 local img_react_test = gfx.image.new('images/react_test')
 local img_timer = gfx.image.new('images/timer')
 
+-- Racism
 class('race').extends(gfx.sprite)
 function race:init()
     race.super.init(self)
 end
 
+-- The track's collision sprite
 class('track_col').extends(gfx.sprite)
 function track_col:init()
     track_col.super.init(self)
@@ -37,6 +40,7 @@ function track_col:init()
     self:add()
 end
 
+-- The track sprite
 class('track_bg').extends(gfx.sprite)
 function track_bg:init()
     track_bg.super.init(self)
@@ -45,6 +49,7 @@ function track_bg:init()
     self:add()
 end
 
+-- The bote! :)
 class('boat').extends(gfx.sprite)
 function boat:init()
     boat.super.init(self)
@@ -78,6 +83,7 @@ function boat:update()
     gfx.setDrawOffset(-self.x+200, -self.y+120)
 end
 
+-- The foreground track elements
 class('track_fg').extends(gfx.sprite)
 function track_fg:init()
     track_fg.super.init(self)
@@ -86,6 +92,7 @@ function track_fg:init()
     self:add()
 end
 
+-- The power meter around the boat
 class('meter').extends(gfx.sprite)
 function meter:init()
     meter.super.init(self)
@@ -108,6 +115,7 @@ function meter:update()
     self:setImage(meter_image, gfx.kImageFlippedY)
 end
 
+-- Bottom-right UI junk
 class('react').extends(gfx.sprite)
 function react:init()
     react.super.init(self)
@@ -118,6 +126,7 @@ function react:init()
     self:add()
 end
 
+-- Top-left UI junk
 class('timer').extends(gfx.sprite)
 function timer:init()
     timer.super.init(self)
@@ -128,6 +137,7 @@ function timer:init()
     self:add()
 end
 
+-- aaaaaaaaaaaaaaaaaagh
 local track_col_sprite = track_col()
 local track_bg_sprite = track_bg()
 local boat_sprite = boat()
